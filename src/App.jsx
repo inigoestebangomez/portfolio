5608f6b1d409f9bb4348bff0d2e139a0a6fef970
Iñigo Esteban <nack_z4@hotmail.com>
2025-10-14 16:51:33 +0200

    Feat: speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home";
 import Work from "./Pages/Work";
 import "./App.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/work/:slug" element={<Work />} />
       </Routes>
+      
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
