c7febc37b5af2a9380987b14bc152d0108eb679a
Iñigo Esteban <nack_z4@hotmail.com>
2026-02-17 09:24:51 +0100

    Fix: breack point for smaller screens

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -20,7 +20,7 @@ export const projects = [
     slug: "wine-tasting-notes",
     services: [
       { name: "iOS", url: "https://apps.apple.com/es/app/wine-tasting-notes/id6758449813" },
-      { name: "Android", url: "https://play.google.com/store/apps/details?id=com.inigoesteban.winetastingnotes" }
+      { name: "Android", url: "https://play.google.com/store/apps/details?id=com.inigo.winetastingnotes" }
     ],
     credits: [
       { role: "Developer", person: "Iñigo Esteban" }
